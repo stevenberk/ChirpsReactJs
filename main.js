70646e7394cde64fdaff963f1d353b3db93853e6
Steven Berk <steven.e.berk@gmail.com>
2018-09-14 14:02:28 -0400

    Content is imported from an API, placeholder array is no longer the default

--- main.js
+++ main.js
@@ -2,76 +2,88 @@ let h = React.createElement;
 
 let generateId = 4;
 
-let EnterChirp = (props) =>{
+let EnterContent = (props) =>{
     return <div>
-        <h2> {props.chirp} </h2>
+        <h2> {props.content} </h2>
         {/* <h3> {props.userId} </h3> */}
         </div> ;
 }
 
-let MapChirps = (props) =>{
+let MapContents = (props) =>{
     return <div>
-            {props.chirplist.map(chirp =>
-            <EnterChirp chirp={chirp.chirp.toUpperCase()} key={chirp.userId}/>)}
+            {props.Contentlist.map(content =>
+            <EnterContent content={content.content.toUpperCase()} key={content.userId}/>)}
          </div> ;
 }
-class CreateChirp extends React.Component {
+class CreateContent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            newChirp: ""
+            newContent: ""
         }
     }
 render(){
     return <form onSubmit={(event) => {
         event.preventDefault();
         event.target.value;
-        this.props.addChirp(this.state.newChirp);
+        this.props.addContent(this.state.newContent);
         }}>
         <input type="text" 
-                value= {this.state.newChirp}
+                value= {this.state.newContent}
                 onChange={(event) => 
-                    this.setState({newChirp: event.target.value})
+                    this.setState({newContent: event.target.value})
             }/> 
          </form> 
 }
 }
 
-let JSX = <h1>wassup</h1>;
+
+
+let JSX = <h1>whats Upper</h1>;
     
 class MyComponent extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
         myArray: [
-            {
-            "userId": 1,
-            "chirp": "Hey! This is the first Chirp"
-            },
-            {
-            "userId": 2,
-            "chirp": "No bots please"
-            },
-            {
-            "userId": 3,
-            "chirp": "Hello whirled"
-            }
+        // this is just a placeholder array
+            // {
+            // "userId": 1,
+            // "id": 0,
+            // "author": "null in a string",
+            // "content": "Hey! This is the first post"
+            // }
         ]
       };
+        let getContent = () =>{
+            // API to an array
+          return fetch('http://0.tcp.ngrok.io:18229/wassups.json')
+          .then(res => res.json())
+          .then(wassups => {
+              this.setState({
+                  myArray: wassups
+              })
+          }
+          )
+        //   .then(data => console.log(data))
+      }
+      getContent();
     }
         render(){ 
-            let addChirp = (chirpcontent) =>{
+            let addContent = (Contentcontent) =>{
                 let newUserId = 4;
                 let newMyArray = this.state.myArray.concat({
                     userId: newUserId,
-                    chirp: chirpcontent.toUpperCase()});
+                    id: 8,
+                    author: "null in a string",
+                    content: Contentcontent.toUpperCase()});
                     this.setState({
                     myArray: newMyArray
                 })
             };
         return h("div", null,  
-            h(CreateChirp, {addChirp: addChirp}),
-            h(MapChirps, {chirplist: this.state.myArray}),
+            h(CreateContent, {addContent: addContent}),
+            h(MapContents, {Contentlist: this.state.myArray}),
     )
 }
 }
